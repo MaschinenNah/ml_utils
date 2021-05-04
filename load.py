e79e5230f7b696cae415187bc28617588707002a
MaschinenNah <maschinennah@gmail.com>
2021-05-04 07:40:55 +0000

    ___

--- load.py
+++ load.py
@@ -3,6 +3,7 @@ from urllib.request import urlopen
 from zipfile import ZipFile
 from PIL import Image
 import numpy as np
+import os
 
 def load_and_unzip_from_url(zipurl):
   with urlopen(zipurl) as zipresp:
@@ -13,3 +14,7 @@ def img_path_to_np_array(path):
   img = Image.open(path)
   img_as_array = np.array(img)
   return (img_as_array / 255.0).astype("float32")
+
+def all_abs_paths_in_dir(dir_):
+  filenames = os.listdir(dir_)
+  return [os.path.join(dir_, filename) for filename in filenames]
