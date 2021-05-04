8e3d12b4a2664f0fc9518ff523dce10409f0a9e5
MaschinenNah <maschinennah@gmail.com>
2021-05-04 04:55:35 +0000

    Commit Nachricht

--- convert.py
+++ convert.py
@@ -1,4 +1,10 @@
-print("convert geladen")
+import numpy as np
 
-def convert():
-  print("convert!")
+def rgb_to_grayscale(np_array):
+  return np.dot(np_array, [0.33, 0.33, 0.33])
+
+def grayscale_to_rgb(np_array):
+  return np.stack((np_array,)*3, axis=-1)
+
+def add_color_channel(np_array):
+  return np_array[..., None]
--- load.py
+++ load.py
@@ -1,4 +1,14 @@
-print("load geladen")
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+from PIL import Image
 
-def load():
-  print("load!")
+def load_and_unzip_from_url(zipurl):
+  with urlopen(zipurl) as zipresp:
+    with ZipFile(BytesIO(zipresp.read())) as zfile:
+      zfile.extractall()
+
+def img_path_to_np_array(path):
+  img = Image.open(path)
+  img_as_array = np.array(img)
+  return (img_as_array / 255.0).astype("float32")
