83d2da6603c020aede4aec69f7f4a5857d2dc34e
MaschinenNah <maschinennah@gmail.com>
2021-05-05 08:19:54 +0000

    funktion commit to github hinzugefügt

--- load.py
+++ load.py
@@ -4,6 +4,7 @@ from zipfile import ZipFile
 from PIL import Image
 import numpy as np
 import os
+import getpass
 
 def load_and_unzip_from_url(zipurl):
   with urlopen(zipurl) as zipresp:
@@ -19,10 +20,18 @@ def all_abs_paths_in_dir(dir_):
   filenames = os.listdir(dir_)
   return [os.path.join(dir_, filename) for filename in filenames]
 
-# veränderung 1
 
-# veränderung 2
 
-# veränderung 3
+def commit_to_github(file):
+  github_pw = getpass.getpass();
+  %cd /content/ml_utils
+  !git config --global user.email "maschinennah@gmail.com"
+  !git config --global user.name "MaschinenNah"
+  !git add $file
+  !git commit -m "neu!"
+  !git remote rm origin
+  !git remote add origin https://MaschinenNah:{github_pw}@github.com/MaschinenNah/ml_utils.git
+  !git push -u origin main
+  %cd /content
 
-# veränderung 4
+commit_to_github("frame_prediction.py");
