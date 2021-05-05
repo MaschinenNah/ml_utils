350d1f1dbb73f978de549c4b4391751e22300b33
MaschinenNah <maschinennah@gmail.com>
2021-05-05 08:55:20 +0000

    neu!

--- load.py
+++ load.py
@@ -19,17 +19,3 @@ def img_path_to_np_array(path):
 def all_abs_paths_in_dir(dir_):
   filenames = os.listdir(dir_)
   return [os.path.join(dir_, filename) for filename in filenames]
-
-
-
-def commit_to_github(file):
-  github_pw = getpass.getpass();
-  %cd /content/ml_utils
-  !git config --global user.email "maschinennah@gmail.com"
-  !git config --global user.name "MaschinenNah"
-  !git add $file
-  !git commit -m "neu!"
-  !git remote rm origin
-  !git remote add origin https://MaschinenNah:{github_pw}@github.com/MaschinenNah/ml_utils.git
-  !git push -u origin main
-  %cd /content
